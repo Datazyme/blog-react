eb20e0454dfa33c50cde5e477acf5317d76938d6
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mini.hitronhub.home>
2024-04-25 23:36:51 -0700

    with UserProfile avatar and name, styled no form yet

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         <img src={logo} alt="Navbar Logo"></img>
       </Link>
       <ul className='nav__menu'>
-        <li><Link to="/profile">Ernest Achiever</Link></li>
+        <li><Link to="/profile/asdfdf">Ernest Achiever</Link></li>
         <li><Link to="/create">Create Post</Link></li>
         <li><Link to="/authors">Authors</Link></li>
         <li><Link to="/logout">Logout</Link></li>
--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+import Avatar6 from '../images/avatar6.jpg'
+import { FaEdit, FaCheck } from "react-icons/fa";
 
 const UserProfile = () => {
+  const [avatar, setAvatar] = useState('')
   return (
-    <div>UserProfile</div>
+    <section className='profile'>
+      <div className='container profile__container'>
+        <Link to={`/myposts/sdfsdf`} className='btn'>My Posts</Link>
+        <div className='profile__details'>
+          <div className='avatar__wrapper'>
+            <div className='profile__avatar'>
+              <img src={Avatar6} alt=""></img>
+            </div>
+            {/* Form to update avatar */}
+            <form className='avatar__form'>
+              <input type='file' name='avatar' id='avatar' onChange={e => setAvatar(e.target.files[0])} accept="png, jpg, jpeg"></input>
+              <label htmlFor='avatar'><FaEdit></FaEdit></label>
+            </form>
+            <button className='profile__avatar-btn'><FaCheck></FaCheck></button>
+          </div>
+          <h1>Red Baron</h1>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
