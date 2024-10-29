733fd2a0bb47299c4ed0c055789545547c6f1a00
Anastasia Kuzmin <kuzminanastasia@Kuzmins-Mini.hitronhub.home>
2024-10-28 20:35:22 -0700

    changing url in index in app.use

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json({extended: true}));
 app.use(express.urlencoded({extended: true}));
 //connects to local host of client folder
-app.use(cors({credentials: true, origin: "https://blog-react-three-nu.vercel.app"}));
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
 app.use(upload());
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
@@ -23,5 +23,5 @@ app.use('/api/posts', postRoutes)
 app.use(notFound);
 app.use(errorHandler);
 
-connect(process.env.MONGO_URI).then(app.listen(process.env.PORT  || 8000, () => console.log(`listening on 
-port ${process.env.PORT}`))).catch(error => {console.log(error)})
+// connect(process.env.MONGO_URI).then(app.listen(process.env.PORT || 8000, () => console.log(`listening on 
+// port ${process.env.PORT}`))).catch(error => {console.log(error)})
